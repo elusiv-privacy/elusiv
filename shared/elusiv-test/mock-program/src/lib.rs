fad7d57a7948571f249ef2d1e1adf5e52235b8a3
pierre-l <pierre.larger@gmail.com>
2023-02-27 15:54:21 +0100

    Add the `no-entrypoint` feature for mock-program
    Fixes the "duplicate entrypoint" issues

--- shared/elusiv-test/mock-program/src/lib.rs
+++ shared/elusiv-test/mock-program/src/lib.rs
@@ -1,5 +1,6 @@
 use solana_program::{pubkey::Pubkey, account_info::AccountInfo, entrypoint::ProgramResult};
 
+#[cfg(not(feature = "no-entrypoint"))]
 solana_program::entrypoint!(process_instruction);
 
 pub fn process_instruction(_: &Pubkey, _: &[AccountInfo], _: &[u8]) -> ProgramResult {
